6342844bb8b99f16f5c47c2541e9fc32dafb59e0
MrShokh4346 <shokhruxabdurasulov0@gmail.com>
2023-02-02 10:36:34 +0500

    added form

--- hello.py
+++ hello.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'this is my secter key'
+
+class NameForm(FlaskForm):
+    name = StringField('Your name:', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+    return render_template('name.html', name=name, form=form)
 
 @app.route('/')
 def index():
