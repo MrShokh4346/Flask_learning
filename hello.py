e0be6d44854b550b5581c9c0c5747459c4b86a76
MrShokh4346 <shokhruxabdurasulov0@gmail.com>
2023-02-02 19:31:25 +0500

    added database

--- hello.py
+++ hello.py
@@ -11,7 +11,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "this is my secter key"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 
 db = SQLAlchemy(app)
 
@@ -20,13 +19,40 @@ class Users(db.Model):
     username = db.Column(db.String(200), nullable=True)
     email = db.Column(db.String(120), nullable=True, unique=True)
     date_joined = db.Column(db.DateTime, default=datetime.utcnow)
+    
     def __repr__(self):
         return f"<Name {self.name}>"
+
+with app.app_context():
+    db.create_all()
+    db.session.commit()
 
 class NameForm(FlaskForm):
     name = StringField('Your name:', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+class UserForm(FlaskForm):
+    name = StringField('Name:', validators=[DataRequired()])
+    email = StringField('Email:', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        # user = Users.query.filter_by(email=form.email.data).first()
+        user = db.session.execute(db.select(Users).filter_by(email=form.email.data)).scalar()
+        if user is None:
+            user = Users(username=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        flash("User Created Successfully")
+    # all_users = Users.query.order_by(Users.date_joined)
+    all_users = db.session.execute(db.select(Users).order_by(Users.date_joined)).scalars()
+    print(all_users)
+    return render_template('add_user.html', name=name, form=form, all_users=all_users)
 
 @app.route('/name', methods=['GET', 'POST'])
 def name():
