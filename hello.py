a13316f0e997e968da4748db141ad6777d0fb0ef
MrShokh4346 <shokhruxabdurasulov0@gmail.com>
2023-02-03 22:17:36 +0500

    added test_pw

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, EqualTo
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
@@ -24,6 +25,18 @@ class Users(db.Model):
     email = db.Column(db.String(120), nullable=True, unique=True)
     color = db.Column(db.String(50))
     date_joined = db.Column(db.DateTime, default=datetime.utcnow)
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError("Passwprd was unrreadable")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
     
     def __repr__(self):
         return f"<Name {self.name}>"
@@ -36,10 +49,17 @@ class NameForm(FlaskForm):
     name = StringField('Your name:', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+class TestForm(FlaskForm):
+    email = StringField('Your email:', validators=[DataRequired()])
+    password = PasswordField('Your password:', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 class UserForm(FlaskForm):
     name = StringField('Name:', validators=[DataRequired()])
     email = StringField('Email:', validators=[DataRequired()])
     color = StringField('Color')
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', message='Passwords Must Much!')])
+    password2 = PasswordField('Comfirm password', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -87,17 +107,21 @@ def update(id):
 def add_user():
     name = None
     form = UserForm()
+    # all_users = Users.query.order_by(Users.date_joined)
+    all_users = db.session.execute(db.select(Users).order_by(Users.date_joined)).scalars()
     if form.validate_on_submit():
         # user = Users.query.filter_by(email=form.email.data).first()
         user = db.session.execute(db.select(Users).filter_by(email=form.email.data)).scalar()
         if user is None:
-            user = Users(username=form.name.data, email=form.email.data, color=form.color.data)
+            hashed_pw = generate_password_hash(form.password.data, 'sha256')
+            user = Users(username=form.name.data, email=form.email.data, color=form.color.data, password_hash=hashed_pw)
             db.session.add(user)
             db.session.commit()
-        name = form.name.data
-        flash("User Created Successfully")
-    # all_users = Users.query.order_by(Users.date_joined)
-    all_users = db.session.execute(db.select(Users).order_by(Users.date_joined)).scalars()
+            name = form.name.data
+            flash("User Created Successfully")
+        else:
+            flash("This Email Is Already Exists")
+            return render_template('add_user.html', name=name, form=form, all_users=all_users)
     return render_template('add_user.html', name=name, form=form, all_users=all_users)
 
 
@@ -110,6 +134,24 @@ def name():
         name = form.name.data
         flash("Form Submitted Successfully")
     return render_template('name.html', name=name, form=form)
+
+
+@app.route('/test_pw', methods=['GET', 'POST'])
+def test_pw():
+    email = None
+    password = None
+    passed = None
+    user = None
+    form = TestForm()
+    if form.validate_on_submit():
+        # user = db.session.execute(db.select(Users).filter_by(email=form.email.data).scelar())
+        user = Users.query.filter_by(email=form.email.data).first()
+        email = form.email.data
+        password = form.password.data
+        passed = check_password_hash(user.password_hash, password)
+        flash("Form Submitted Successfully")
+    return render_template('test_pw.html',passed=passed,
+     email=email, form=form, user=user, pw=password)
 
 
 
