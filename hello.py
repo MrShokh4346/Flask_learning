495d5d58580f7469013801b3811e5b21d51d431d
MrShokh4346 <shokhruxabdurasulov0@gmail.com>
2023-02-04 11:42:39 +0500

    added blog post

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, EqualTo
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms.widgets import TextArea
 
 
 app = Flask(__name__)
@@ -21,8 +22,8 @@ migrate = Migrate(app, db)
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(200), nullable=True)
-    email = db.Column(db.String(120), nullable=True, unique=True)
+    username = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
     color = db.Column(db.String(50))
     date_joined = db.Column(db.DateTime, default=datetime.utcnow)
     password_hash = db.Column(db.String(128))
@@ -45,6 +46,24 @@ with app.app_context():
     db.create_all()
     db.session.commit()
 
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    body = db.Column(db.Text(), nullable=False)
+    author = db.Column(db.String(200), nullable=False)
+    slug = db.Column(db.String(255), nullable=False)
+    posted_date =db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"{self.title}"
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    body = StringField('Body', validators=[DataRequired()], widget=TextArea())
+    slug = StringField('Slug', validators=[DataRequired()])
+    author = StringField("Author", validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 class NameForm(FlaskForm):
     name = StringField('Your name:', validators=[DataRequired()])
     submit = SubmitField('Submit')
@@ -62,7 +81,83 @@ class UserForm(FlaskForm):
     password2 = PasswordField('Comfirm password', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+@app.route('/delete/<int:id>')
+def delete_post(id):
+    try:
+        post = Posts.query.get_or_404(id)
+        db.session.delete(post)
+        db.session.commit()
+        flash('Post deleted')
+        posts = Posts.query.order_by(Posts.posted_date)
+        return render_template('posts.html', posts=posts)
+    except:
+        flash('Somethoing went wrong. Try again...')
+        posts = Posts.query.order_by(Posts.posted_date)
+        return render_template('posts.html', posts=posts)
 
+@app.route('/post/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    post = Posts.query.get_or_404(id)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.body = form.body.data
+        post.slug = form.slug.data
+        post.author = form.author.data
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('post', id=id))
+    form.title.data = post.title
+    form.body.data = post.body
+    form.slug.data = post.slug
+    form.author.data = post.author
+    return render_template('edit_post.html', form=form)
+    
+@app.route('/post/<int:id>')
+def post(id):
+    post = Posts.query.get_or_404(id)
+    return render_template('post.html', post=post)
+
+
+@app.route('/posts')
+def posts():
+    posts = Posts.query.order_by(Posts.posted_date)
+    return render_template('posts.html', posts=posts)
+
+@app.route('/add_post', methods=['GET', 'POST'])
+def add_post():
+    form = PostForm()
+    
+    # if form.validate_on_submit():
+    #     post = Posts(
+    #         title=form.title.data,
+    #         body=form.body.data,
+    #         slug=form.slug.data,
+    #         author=form.author.data
+    #     )
+    #     db.session.add(post)
+    #     db.session.commit()
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        slug = request.form['slug']
+        author = request.form['author']
+        print(title, body, author, slug)
+        print('hello')
+        post = Posts(
+            title = title,
+            body = body,
+            slug = slug,
+            author = author
+        )
+        db.session.add(post)
+        db.session.commit()
+        form.title.data = ''
+        form.body.data = ''
+        form.slug.data = ''
+        form.author.data = ''
+    posts = Posts.query.all()
+    return render_template('add_post.html', form=form, posts=posts)
 
 @app.route('/delete/<int:id>', methods=['GET', 'POST'])
 def delete(id):
@@ -118,6 +213,10 @@ def add_user():
             db.session.add(user)
             db.session.commit()
             name = form.name.data
+            form.name.data = ''
+            form.name.email = ''
+            form.name.color = ''
+            form.name.password = ''
             flash("User Created Successfully")
         else:
             flash("This Email Is Already Exists")
